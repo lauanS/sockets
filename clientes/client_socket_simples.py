d5fd77a458746faaac535b72f6032514a1b793c3
lauanS <lauan.s.souza@gmail.com>
2019-09-25 00:10:32 -0300

    Atualizando o cliente para enviar um arquivo codificado na BASE64

--- clientes/client_socket_simples.py
+++ clientes/client_socket_simples.py
@@ -1,4 +1,25 @@
 import socket
+import base64
+
+
+def encode_64():
+    input = open('/home/lauans/law/gitRep/sockets/img_sockets.jpeg', 'rb')
+    output = open('/home/lauans/law/gitRep/sockets/txt_img.txt', 'wb')
+
+    base64.encode(input, output)
+
+    input.close()
+    output.close()
+
+    with open('/home/lauans/law/gitRep/sockets/txt_img.txt', 'rb') as file:
+        base64_str = file.read()
+
+    return base64_str
+
+
+msg = b'{ "type":"write", "file":"new_file.jpeg", "msg":"'
+msg += encode_64()
+msg += b'"}'
 
 # criando o socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -6,9 +27,6 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Onde nosso socket servidor estará escutando
 client_socket.connect(("localhost", 2077))
 
-msg = b'{ "type":"write", "file":"new_file.txt", "msg":"'
-msg += b'Quando eu me encontrava na'
-msg += b'"}'
 file_name = b'teste.txt'
 
 totalsent = 0
